ee392e8f00e28f0023b17866f74c98f81cf50ebc
Kathelyn Gaioni <112026988+kathelyngaioni@users.noreply.github.com>
2022-11-07 16:47:59 +0000

    Primeira versão do arquivo intruso.cpp

---
+++ .ccls-cache/@home@runner@VPL07-CaixaEletronico-1/intruso.cpp
@@ -0,0 +1,30 @@
+#include "intruso.hpp"
+#include <sstream>
+#include <vector>
+
+void Intruso::set_senha_vazada(std::string vazou){
+entradas.insert(vazou);
+}
+std::string Intruso::crack_senha(){
+std::set<std::string>sequencias;
+int associcao_caixa;
+std::string nova_associacao;
+std::string letras;
+std::istringstream stream_string;
+
+//percorrer o set e pegar a parte da associacao do caixa
+for(std::string _senha : entradas){
+stream_string = std::istringstream(_senha);
+  stream_string >> associcao_caixa;
+stream_string >> letras;
+std::string associacao = std::to_string(associcao_caixa);
+
+  for(int i=0;i<letras.size();i++){
+  if(letras[i]=='A') nova_associacao += associacao[0] + associacao[1];
+  if(letras[i]=='B') nova_associacao += associacao[2] + associacao[3];
+  if(letras[i]=='C') nova_associacao += associacao[4] + associacao[5];
+  if(letras[i]=='D') nova_associacao += associacao[6] + associacao[7];
+  if(letras[i]=='E') nova_associacao += associacao[8] + associacao[9];    
+   } 
+}  return "";
+}
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 O arquivo main.cpp já estava pronto e não era para ser modificado. Porém, a função std::cin.ignore() dentro do for fazia com que as entradas a serem testadas não "entrarem" corretamente.
 
 No arquivo intruso.hpp, foi criado um set de strings para armazenar as entradas (senhas) do programa.
+
+A primeira ideia para a criação do arquivo intruso.cpp é armazenar as entradas no set entradas. Pecorrer cada entrada do set e separar os números das letras. A partir disso, é criada uma nova_associacao. 
